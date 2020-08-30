cdf56e9afc3a1ab6d2b00422c0508a44e931de5f
Troy Boettger <troy.boettger@gmail.com>
2020-08-30 15:55:34 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alet alert-info">Info: Click on a Quote to delete</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from "./components/QuoteGrid";
+import NewQuote from "./components/NewQuote";
+import Header from "./components/Header";
+
+export default {
+  data: function () {
+    return {
+      quotes: ["something to see something"],
+      maxQuotes: 10,
+    };
+  },
+  methods: {
+    newQuote(quote) {
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    },
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header,
+  },
+};
+</script>
+
+<style>
+</style>
